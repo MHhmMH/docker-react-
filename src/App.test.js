a6fd7cdaca59663ef68b6a4a56a009998ad647eb
hanminghao <hmh950728@126.com>
2020-01-02 22:23:07 -0800

    fit test

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/Hi Minghao/i);
+  const linkElement = getByText(/I am change from feature branch/i);
   expect(linkElement).toBeInTheDocument();
 });
